42d871f832cf5ae71a5a79dc7faba06b0afc923c
JihyunKim <jihyun@squarelab.co>
2019-12-10 11:13:29 +0900

    Update

--- _site/js/interaction.js
+++ _site/js/interaction.js
@@ -14,14 +14,14 @@ let supportsCssVars = function () {
 if (!supportsCssVars()) alert('Please view this demo in a modern browser that supports CSS Variables.')
 
 //Scroll Magic
-window.onload = function () { // 윈도우가 다 열리길 기다리는 구문
+window.onload = function () { // 윈도우 로드를 기다리는 구문
     // Init ScrollMagic
     let controller = new ScrollMagic.Controller({
         refreshInterval: 0
     });
 
     // Logo
-    let logo = new ScrollMagic.Scene({
+    new ScrollMagic.Scene({
         triggerElement: '#playwings',
         triggerHook: 0.05
     })
@@ -35,13 +35,102 @@ window.onload = function () { // 윈도우가 다 열리길 기다리는 구문
         .from('.recruit-wrapper', 1, {autoAlpha: 0, ease: Power0.easeNone})
         .from('.recruit-bg', 1, {y: '-40%', ease: Power0.easeNone})
 
-    let slideParallaxScene = new ScrollMagic.Scene({
+    new ScrollMagic.Scene({
         triggerElement: '#recruit',
         triggerHook: 1,
         duration: '90%'
     })
         .setTween(parallaxTl)
         .addTo(controller);
+
+    imagesLoaded(document.body, () => {
+        anime.remove(DOM.svg);
+        anime({
+            targets: DOM.svg,
+            duration: 1,
+            easing: 'linear',
+            scaleX: shapes[0].scaleX,
+            scaleY: shapes[0].scaleY,
+            translateX: shapes[0].tx + 'px',
+            translateY: shapes[0].ty + 'px',
+            rotate: shapes[0].rotate + 'deg'
+        });
+
+        let step;
+        DOM.contentElems.forEach((el, pos) => {
+            const scrollElemToWatch = pos ? DOM.contentElems[pos] : DOM.contentElems;
+            pos = pos ? pos : contentElemsTotal;
+            const watcher = scrollMonitor.create(scrollElemToWatch, -350);
+
+            watcher.enterViewport(function () {
+                step = pos;
+                anime.remove(DOM.shapeEl);
+                anime({
+                    targets: DOM.shapeEl,
+                    duration: shapes[pos].animation.points.duration,
+                    easing: shapes[pos].animation.points.easing,
+                    elasticity: shapes[pos].animation.points.elasticity || 0,
+                    points: shapes[pos].points,
+                    fill: {
+                        value: shapes[pos].fill.color,
+                        duration: shapes[pos].fill.duration,
+                        easing: shapes[pos].fill.easing
+                    }
+                });
+
+                anime.remove(DOM.svg);
+                anime({
+                    targets: DOM.svg,
+                    duration: shapes[pos].animation.svg.duration,
+                    easing: shapes[pos].animation.svg.easing,
+                    elasticity: shapes[pos].animation.svg.elasticity || 0,
+                    scaleX: shapes[pos].scaleX,
+                    scaleY: shapes[pos].scaleY,
+                    translateX: shapes[pos].tx + 'px',
+                    translateY: shapes[pos].ty + 'px',
+                    rotate: shapes[pos].rotate + 'deg'
+                });
+            });
+
+            watcher.exitViewport(function () {
+                const idx = !watcher.isAboveViewport ? pos - 1 : pos + 1;
+
+                if (idx <= contentElemsTotal && step !== idx) {
+                    step = idx;
+                    anime.remove(DOM.shapeEl);
+                    anime({
+                        targets: DOM.shapeEl,
+                        duration: shapes[idx].animation.points.duration,
+                        easing: shapes[idx].animation.points.easing,
+                        elasticity: shapes[idx].animation.points.elasticity || 0,
+                        points: shapes[idx].points,
+                        fill: {
+                            value: shapes[idx].fill.color,
+                            duration: shapes[idx].fill.duration,
+                            easing: shapes[idx].fill.easing
+                        }
+                    });
+
+                    anime.remove(DOM.svg);
+                    anime({
+                        targets: DOM.svg,
+                        duration: shapes[idx].animation.svg.duration,
+                        easing: shapes[idx].animation.svg.easing,
+                        elasticity: shapes[idx].animation.svg.elasticity || 0,
+                        scaleX: shapes[idx].scaleX,
+                        scaleY: shapes[idx].scaleY,
+                        translateX: shapes[idx].tx + 'px',
+                        translateY: shapes[idx].ty + 'px',
+                        rotate: shapes[idx].rotate + 'deg'
+                    });
+                }
+            });
+        });
+
+        Array.from(document.querySelectorAll('.content--layout')).forEach(el => new TiltObj(el));
+        // Remove loading class from body
+        document.body.classList.remove('loading');
+    });
 }
 
 const extend = function (a, b) {
@@ -226,7 +315,7 @@ const shapes = [
         }
     },
     {
-        points: '315.61 0.62 6.62 229.5 120.92 599.83 510.31 599.83 630.64 229.5 315.61 0.62', //오각
+        points: '358.01 0 736.02 267.38 595.46 700 140.57 700 10 267.38 358.01 0', //오각
         scaleX: 1,
         scaleY: 1,
         rotate: 90,
@@ -295,102 +384,3 @@ const shapes = [
         }
     }
 ];
-let step;
-
-const initShapeEl = function () {
-    anime.remove(DOM.svg);
-    anime({
-        targets: DOM.svg,
-        duration: 1,
-        easing: 'linear',
-        scaleX: shapes[0].scaleX,
-        scaleY: shapes[0].scaleY,
-        translateX: shapes[0].tx + 'px',
-        translateY: shapes[0].ty + 'px',
-        rotate: shapes[0].rotate + 'deg'
-    });
-};
-
-const createScrollWatchers = function () {
-    DOM.contentElems.forEach((el, pos) => {
-        const scrollElemToWatch = pos ? DOM.contentElems[pos] : DOM.contentElems;
-        pos = pos ? pos : contentElemsTotal;
-        const watcher = scrollMonitor.create(scrollElemToWatch, -350);
-
-        watcher.enterViewport(function () {
-            step = pos;
-            anime.remove(DOM.shapeEl);
-            anime({
-                targets: DOM.shapeEl,
-                duration: shapes[pos].animation.points.duration,
-                easing: shapes[pos].animation.points.easing,
-                elasticity: shapes[pos].animation.points.elasticity || 0,
-                points: shapes[pos].points,
-                fill: {
-                    value: shapes[pos].fill.color,
-                    duration: shapes[pos].fill.duration,
-                    easing: shapes[pos].fill.easing
-                }
-            });
-
-            anime.remove(DOM.svg);
-            anime({
-                targets: DOM.svg,
-                duration: shapes[pos].animation.svg.duration,
-                easing: shapes[pos].animation.svg.easing,
-                elasticity: shapes[pos].animation.svg.elasticity || 0,
-                scaleX: shapes[pos].scaleX,
-                scaleY: shapes[pos].scaleY,
-                translateX: shapes[pos].tx + 'px',
-                translateY: shapes[pos].ty + 'px',
-                rotate: shapes[pos].rotate + 'deg'
-            });
-        });
-
-        watcher.exitViewport(function () {
-            const idx = !watcher.isAboveViewport ? pos - 1 : pos + 1;
-
-            if (idx <= contentElemsTotal && step !== idx) {
-                step = idx;
-                anime.remove(DOM.shapeEl);
-                anime({
-                    targets: DOM.shapeEl,
-                    duration: shapes[idx].animation.points.duration,
-                    easing: shapes[idx].animation.points.easing,
-                    elasticity: shapes[idx].animation.points.elasticity || 0,
-                    points: shapes[idx].points,
-                    fill: {
-                        value: shapes[idx].fill.color,
-                        duration: shapes[idx].fill.duration,
-                        easing: shapes[idx].fill.easing
-                    }
-                });
-
-                anime.remove(DOM.svg);
-                anime({
-                    targets: DOM.svg,
-                    duration: shapes[idx].animation.svg.duration,
-                    easing: shapes[idx].animation.svg.easing,
-                    elasticity: shapes[idx].animation.svg.elasticity || 0,
-                    scaleX: shapes[idx].scaleX,
-                    scaleY: shapes[idx].scaleY,
-                    translateX: shapes[idx].tx + 'px',
-                    translateY: shapes[idx].ty + 'px',
-                    rotate: shapes[idx].rotate + 'deg'
-                });
-            }
-        });
-    });
-};
-
-const init = function () {
-    imagesLoaded(document.body, () => {
-        initShapeEl();
-        createScrollWatchers();
-        Array.from(document.querySelectorAll('.content--layout')).forEach(el => new TiltObj(el));
-        // Remove loading class from body
-        document.body.classList.remove('loading');
-    });
-}
-
-init();
