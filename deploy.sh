cfb1cf91732b7ecd72d15d69435f5be31c3b1e63
yjiq150 <yjiq150@gmail.com>
2022-11-11 17:47:40 +0900

    Fix deploy script to use awscli instead of s3cmd

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ cp _site/blog.html _site/blog/index.html
 echo "Deploying to PRD"
 DEPLOY_URL=s3://squarelab.co
 
-# --guess-mime-type --no-mime-magic is required. If not specified, css files that are uploaded to S3 will have "text/plain" MIME type.
-s3cmd sync --exclude-from .s3ignore _site/ $DEPLOY_URL --delete-removed --guess-mime-type --no-mime-magic $@
+aws s3 sync _site/ $DEPLOY_URL --delete --acl 'public-read'
