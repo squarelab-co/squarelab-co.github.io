82945196bac2849acc8c7e3f22f0432b1636a363
yjiq150 <yjiq150@gmail.com>
2023-10-16 10:38:49 +0900

    Remove ACL option in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ cp _site/blog.html _site/blog/index.html
 echo "Deploying to PRD"
 DEPLOY_URL=s3://squarelab.co
 
-aws s3 sync _site/ $DEPLOY_URL --delete --acl 'public-read'
+aws s3 sync _site/ $DEPLOY_URL --delete
